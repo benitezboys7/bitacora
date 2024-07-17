90a10ba0e20a5f82c9a240130d2b92742a4fa66f
benitezboys7 <cesart356@gmail.com>
2024-07-17 13:24:45 -0500

    arreglo php edit

--- edit_customer.php
+++ edit_customer.php
@@ -1,8 +1,9 @@
 <?php
 include 'db.php';
 
+$id = $_GET['id'];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id = $_POST['id'];
     $name = $_POST['name'];
     $email = $_POST['email'];
 
@@ -19,11 +20,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit();
 }
 
-// Solo para pruebas o si quieres ver los detalles del cliente
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM customers WHERE id=$id";
-    $result = $conn->query($sql);
-    $customer = $result->fetch_assoc();
-}
+$sql = "SELECT * FROM customers WHERE id=$id";
+$result = $conn->query($sql);
+$customer = $result->fetch_assoc();
+
 ?>
+
+<form method="post" action="">
+    Name: <input type="text" name="name" value="<?php echo $customer['name']; ?>" required><br>
+    Email: <input type="email" name="email" value="<?php echo $customer['email']; ?>" required><br>
+    <input type="submit" value="Update Customer">
+</form>
