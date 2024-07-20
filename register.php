e367463adf731b1a960d095ea72d640e77910bb4
benitezboys7 <cesart356@gmail.com>
2024-07-20 02:14:09 -0500

    pestaña de agregar usuarios listo, condicion de que solo se puedan cuando se inicie sesion

--- register.php
+++ register.php
@@ -1,4 +1,11 @@
 <?php
+
+if (!isset($_SESSION['user_id'])) {
+    // Redirige al inicio de sesión si no hay una sesión activa
+    header("Location: index.php");
+    exit();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include 'db.php';
 
