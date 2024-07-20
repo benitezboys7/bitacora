373d8785fe2411f8d2b0d9a98a199ec46acb13a7
benitezboys7 <cesart356@gmail.com>
2024-07-20 01:44:23 -0500

    intentando añadir register en la seccion de add product

--- script.js
+++ script.js
@@ -1,51 +1,93 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
     const logoutButton = document.getElementById("logoutButton");
 
-    function showMessage(message) {
+    function showMessage(message, redirect = false) {
         const messageContainer = document.getElementById("messageContainer");
         messageContainer.textContent = message;
         messageContainer.classList.remove("hidden");
 
         setTimeout(() => {
             messageContainer.classList.add("hidden");
-            // En el caso de logout, redirige a la página deseada después de 2 segundos
-            window.location.href = "dashboard.php";
-        }, 1000); // 1000 ms = 1 segundos
+            if (redirect) {
+                window.location.href = redirect;
+            }
+        }, 2000); // 2000 ms = 2 segundos
     }
 
-    if (window.location.pathname.endsWith("index.php") || window.location.pathname === "/") {
-        // Página de inicio de sesión
-        if (loginForm) {
-            loginForm.addEventListener("submit", function(event) {
-                event.preventDefault();
-                const email = document.getElementById("loginEmail").value;
-                const password = document.getElementById("loginPassword").value;
+    // Manejar el formulario de inicio de sesión
+    if (loginForm) {
+        loginForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const email = document.getElementById("loginEmail").value;
+            const password = document.getElementById("loginPassword").value;
 
-                if (!validateEmail(email) || !validatePassword(password)) {
-                    alert("Invalid email or password format");
-                    return;
+            if (!validateEmail(email) || !validatePassword(password)) {
+                alert("Invalid email or password format");
+                return;
+            }
+
+            const formData = new FormData(loginForm);
+            fetch("login.php", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                showMessage(data.message, data.success ? "dashboard.php" : false);
+            })
+            .catch(error => console.error('Error:', error));
+        });
+    }
+
+    // Manejar el formulario de registro
+    if (registerForm) {
+        registerForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            if (!validateEmail(email) || !validatePassword(password)) {
+                alert("Invalid email or password format");
+                return;
+            }
+
+            const formData = new FormData(registerForm);
+            fetch("register.php", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.text())
+            .then(message => {
+                showMessage(message, "/index.php"); // Redirige a la página de inicio de sesión
+            })
+            .catch(error => console.error('Error:', error));
+        });
+    }
+
+    // Manejar el botón de cerrar sesión
+    if (logoutButton) {
+        logoutButton.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            fetch("logout.php", {
+                method: "POST"
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    showMessage("Logout exitoso!", "index.php"); // Redirige a la página de inicio
+                } else {
+                    console.error("Error en el logout: ", data.message);
                 }
+            })
+            .catch(error => console.error('Error:', error));
+        });
+    }
 
-                const formData = new FormData(loginForm);
-                fetch("login.php", {
-                    method: "POST",
-                    body: formData
-                })
-                .then(response => response.json())
-                .then(data => {
-                    showMessage(data.message);
-                    if (data.success) {
-                        setTimeout(() => {
-                            window.location.href = "dashboard.php"; // Redirige a dashboard.php después de mostrar el mensaje
-                        }, 2000);
-                    }
-                })
-                .catch(error => console.error('Error:', error));
-            });
-        }
-    } else if (window.location.pathname.endsWith("dashboard.php")) {
-        // Página del Dashboard
+    // Manejar el dashboard
+    if (window.location.pathname.endsWith("dashboard.php")) {
         const links = document.querySelectorAll("aside a.menu-link");
         const contentSections = document.querySelectorAll(".content-section");
 
@@ -62,6 +104,16 @@ document.addEventListener("DOMContentLoaded", function() {
                     section.style.display = section.id === target ? "block" : "none";
                 });
 
+                // Cargar contenido para "Add Product"
+                if (target === "add-product") {
+                    fetch("register.php")
+                    .then(response => response.text())
+                    .then(html => {
+                        document.getElementById("addProductContent").innerHTML = html;
+                    })
+                    .catch(error => console.error('Error loading register.php:', error));
+                }
+
                 history.pushState(null, '', 'dashboard.php?view=' + target);
             });
         });
@@ -70,26 +122,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const defaultView = getParameterByName('view') || 'dashboard';
         document.getElementById(defaultView).style.display = 'block';
         document.querySelector(`aside a.menu-link[data-target=${defaultView}]`).classList.add('active');
-
-        // Botón de cerrar sesión
-        if (logoutButton) {
-            logoutButton.addEventListener("click", function(event) {
-                event.preventDefault();
-
-                fetch("logout.php", {
-                    method: "POST"
-                })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.success) {
-                        showMessage("Logout exitoso!"); // Muestra el mensaje de logout exitoso
-                    } else {
-                        console.error("Error en el logout: ", data.message);
-                    }
-                })
-                .catch(error => console.error('Error:', error));
-            });
-        }
 
         // Mostrar modal de añadir cliente
         const addCustomerBtn = document.getElementById("addCustomerBtn");
@@ -209,7 +241,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function validatePassword(password) {
-        // Aquí puedes agregar validaciones adicionales para la contraseña si lo deseas
         return password.length >= 6; // Ejemplo: longitud mínima de 6 caracteres
     }
 });
